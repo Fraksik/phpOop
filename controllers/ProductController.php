461db6e862be4d8216d0e4b3bc69c06c85cc7d16
Fraksik <fraks@list.ru>
2018-10-20 21:07:46 +0300

    тест

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -10,19 +10,6 @@ class ProductController extends ControllersModel
 {
 	protected $layout = "main";
 
-    public function run($action = null)
-    {
-        $this->action = $action ?: $this->defaultAction;
-        $method = "action" . ucfirst($this->action);
-        if(method_exists($this, $method)){
-            $this->$method();
-        }else{
-            echo "404";
-        }
-
-    }
-
-
     public function actionIndex()
     {
 	    $model = Product::getAll();
